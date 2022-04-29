b12f7afc820fff97863e77c956fceb3caad680e7
heyanny2 <heyanny2@yandex.ru>
2022-04-29 11:32:04 +0300

    Update progression.js

--- src/games/progression.js
+++ src/games/progression.js
@@ -2,12 +2,12 @@ import randFromInt from '../util.js';
 
 const gameRules = 'What number is missing in the progression?';
 
-const getProgression = (length, pitch) => {
+const getProgression = (length, start, pitch) => {
   const result = [];
-  let start = randFromInt(1, 10);
+  let progStart = start;
   for (let i = 0; i < length; i += 1) {
-    result.push(start);
-    start += pitch;
+    result.push(progStart);
+    progStart += pitch;
   }
   return result;
 };
@@ -15,7 +15,8 @@ const getProgression = (length, pitch) => {
 const generateRound = () => {
   const progressionLength = randFromInt(5, 10);
   const progressionPitch = randFromInt(1, 10);
-  const progression = getProgression(progressionLength, progressionPitch);
+  const progressionStart = randFromInt(1, 10);
+  const progression = getProgression(progressionLength, progressionStart, progressionPitch);
   const randIndex = randFromInt(0, progressionLength - 1);
   const correctAnswer = String(progression[randIndex]);
   progression[randIndex] = '..';
